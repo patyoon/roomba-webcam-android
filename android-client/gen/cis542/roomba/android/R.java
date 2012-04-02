c36d7d3a4e27f9432dee3a54358e8b38ad69fe5b
Patrick Yoon <yeyoon@cis.upenn.edu>
2012-04-02 17:29:58 -0400

    added basic video player. tested with mp4 streaming. not working..

--- android-client/gen/cis542/roomba/android/R.java
+++ android-client/gen/cis542/roomba/android/R.java
@@ -13,11 +13,31 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int downButton=0x7f050007;
+        public static final int gridLayout1=0x7f050002;
+        public static final int leftButton=0x7f050004;
+        public static final int linearLayout1=0x7f050000;
+        public static final int linearLayout2=0x7f050008;
+        public static final int pauseButton=0x7f050009;
+        public static final int quitButton=0x7f05000a;
+        public static final int rightButton=0x7f050006;
+        public static final int stopButton=0x7f050005;
+        public static final int surface=0x7f050001;
+        public static final int upButton=0x7f050003;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int down=0x7f040006;
         public static final int hello=0x7f040000;
+        public static final int left=0x7f040003;
+        public static final int pause=0x7f040007;
+        public static final int quit=0x7f040002;
+        public static final int right=0x7f040004;
+        public static final int stop=0x7f040008;
+        public static final int up=0x7f040005;
     }
 }
--- android-client/src/cis542/roomba/android/MainActivity.java
+++ android-client/src/cis542/roomba/android/MainActivity.java
@@ -1,13 +1,199 @@
 package cis542.roomba.android;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnBufferingUpdateListener;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.media.MediaPlayer.OnVideoSizeChangedListener;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity implements
+OnBufferingUpdateListener, OnCompletionListener,
+OnPreparedListener, OnVideoSizeChangedListener, SurfaceHolder.Callback {
+
+	private static final String TAG = "VideoStreamer";
+	private int mVideoWidth;
+	private int mVideoHeight;
+	private MediaPlayer mMediaPlayer;
+	private SurfaceView mPreview;
+	private SurfaceHolder holder;
+	private String path;
+	private Bundle extras;
+	private static final String MEDIA = "media";
+	private static final int LOCAL_AUDIO = 1;
+	private static final int STREAM_AUDIO = 2;
+	private static final int RESOURCES_AUDIO = 3;
+	private static final int LOCAL_VIDEO = 4;
+	private static final int STREAM_VIDEO = 5;
+	private boolean mIsVideoSizeKnown = false;
+	private boolean mIsVideoReadyToBePlayed = false;
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.main);
+		mPreview = (SurfaceView) findViewById(R.id.surface);
+		holder = mPreview.getHolder();
+		holder.addCallback(this);
+		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+	private void playVideo(Integer Media) {
+		doCleanUp();
+		try {
+			/*
+			 * TODO: Set path variable to progressive streamable mp4 or
+			 * 3gpp format URL. Http protocol should be used.
+			 * Mediaplayer can only play "progressive streamable
+			 * contents" which basically means: 1. the movie atom has to
+			 * precede all the media data atoms. 2. The clip has to be
+			 * reasonably interleaved.
+			 * 
+			 */
+			//example path
+			path = "http://people.sc.fsu.edu/~jburkardt/data/mp4/cvt_size_movie.mp4";
+			if (path == "") {
+				// Tell the user to provide a media file URL.
+				Toast
+				.makeText(
+						MainActivity.this,
+						"Please edit MediaPlayerDemo_Video Activity,"
+								+ " and set the path variable to your media file URL.",
+								Toast.LENGTH_LONG).show();
+
+			}
+
+			// Create a new media player and set the listeners
+			mMediaPlayer = new MediaPlayer();
+			mMediaPlayer.setDataSource(path);
+			mMediaPlayer.setDisplay(holder);
+			mMediaPlayer.prepare();
+			mMediaPlayer.setOnBufferingUpdateListener(this);
+			mMediaPlayer.setOnCompletionListener(this);
+			mMediaPlayer.setOnPreparedListener(this);
+			mMediaPlayer.setOnVideoSizeChangedListener(this);
+			mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+
+		} catch (Exception e) {
+			Log.e(TAG, "error: " + e.getMessage(), e);
+		}
+	}
+
+	public void onBufferingUpdate(MediaPlayer arg0, int percent) {
+		Log.d(TAG, "onBufferingUpdate percent:" + percent);
+
+	}
+
+	public void onCompletion(MediaPlayer arg0) {
+		Log.d(TAG, "onCompletion called");
+	}
+
+	public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+		Log.v(TAG, "onVideoSizeChanged called");
+		if (width == 0 || height == 0) {
+			Log.e(TAG, "invalid video width(" + width + ") or height(" + height + ")");
+			return;
+		}
+		mIsVideoSizeKnown = true;
+		mVideoWidth = width;
+		mVideoHeight = height;
+		if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+			startVideoPlayback();
+		}
+	}
+
+	public void onPrepared(MediaPlayer mediaplayer) {
+		Log.d(TAG, "onPrepared called");
+		mIsVideoReadyToBePlayed = true;
+		if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+			startVideoPlayback();
+		}
+	}
+
+	public void surfaceChanged(SurfaceHolder surfaceholder, int i, int j, int k) {
+		Log.d(TAG, "surfaceChanged called");
+
+	}
+
+	public void surfaceDestroyed(SurfaceHolder surfaceholder) {
+		Log.d(TAG, "surfaceDestroyed called");
+	}
+
+
+	public void surfaceCreated(SurfaceHolder holder) {
+		Log.d(TAG, "surfaceCreated called");
+		playVideo(MainActivity.STREAM_VIDEO);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		releaseMediaPlayer();
+		doCleanUp();
+	}
+
+	private void releaseMediaPlayer() {
+		if (mMediaPlayer != null) {
+			mMediaPlayer.release();
+			mMediaPlayer = null;
+		}
+	}
+
+	private void doCleanUp() {
+		mVideoWidth = 0;
+		mVideoHeight = 0;
+		mIsVideoReadyToBePlayed = false;
+		mIsVideoSizeKnown = false;
+	}
+
+	private void startVideoPlayback() {
+		Log.v(TAG, "startVideoPlayback");
+		holder.setFixedSize(mVideoWidth, mVideoHeight);
+		mMediaPlayer.start();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		releaseMediaPlayer();
+		doCleanUp();
+	}
+	
+	public void pauseButtonListener(View v) {
+		onPause();
+	}
+	
+	public void quitButtonListener(View v) {
+		finish();
+	}
+	
+	/* TODOS. Moving Roomba */
+	public void upButtonListener(View v) {
+		finish();
+	}
+	
+	public void downButtonListener(View v) {
+		finish();
+	}
+	
+	public void leftButtonListener(View v) {
+		finish();
+	}
+	public void rightButtonListener(View v) {
+		finish();
+	}
+	public void stopButtonListener(View v) {
+		finish();
+	}
+	
 }
