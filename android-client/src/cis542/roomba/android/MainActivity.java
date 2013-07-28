6ba2d86bf9d37ba1aaa834a86f76fc6b2950743a
Patrick Yoon <me@patyoon.com>
2013-07-29 01:17:26 +0900

    removed unneeded text files

--- android-client/src/cis542/roomba/android/MainActivity.java
+++ android-client/src/cis542/roomba/android/MainActivity.java
@@ -1,6 +1,5 @@
 package cis542.roomba.android;
 
-
 import android.app.Activity;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -30,22 +29,22 @@ import android.view.WindowManager;
 public class MainActivity extends Activity implements
  SensorEventListener{
 
-	private static final String TAG = "VideoStreamer";
-	private int mVideoWidth;
-	private int mVideoHeight;
-	private MediaPlayer mMediaPlayer;
-	private SurfaceView mPreview;
-	private SurfaceHolder holder;
-	private String path;
-	private static final String MEDIA = "media";
-	private static final int STREAM_VIDEO = 1;
-	private boolean mIsVideoSizeKnown = false;
-	private boolean mIsVideoReadyToBePlayed = false;
-	private SensorManager mSensorManager;
-	private PowerManager mPowerManager;
-	private WindowManager mWindowManager;
-	private Display mDisplay;
-	private WakeLock mWakeLock;
+        private static final String TAG = "VideoStreamer";
+        private int mVideoWidth;
+        private int mVideoHeight;
+        private MediaPlayer mMediaPlayer;
+        private SurfaceView mPreview;
+        private SurfaceHolder holder;
+        private String path;
+        private static final String MEDIA = "media";
+        private static final int STREAM_VIDEO = 1;
+        private boolean mIsVideoSizeKnown = false;
+        private boolean mIsVideoReadyToBePlayed = false;
+        private SensorManager mSensorManager;
+        private PowerManager mPowerManager;
+        private WindowManager mWindowManager;
+        private Display mDisplay;
+        private WakeLock mWakeLock;
     private Sensor mAccelerometer;
     private float mXOrigin;
     private float mYOrigin;
@@ -54,103 +53,103 @@ public class MainActivity extends Activity implements
     private long mSensorTimeStamp;
     private long mCpuTimeStamp;
 
-	/* TCP Client Network Task */
-	NetworkTask networktask;
-	String ipAddress = "";
-	int portNumber = -1;
+        /* TCP Client Network Task */
+        NetworkTask networktask;
+        String ipAddress = "";
+        int portNumber = -1;
 
-	private static final int EDIT_ACTION = 1;
+        private static final int EDIT_ACTION = 1;
 
-	@Override
-	public void onCreate(Bundle bundle) {
-		super.onCreate(bundle);
-		setContentView(R.layout.main);
+        @Override
+        public void onCreate(Bundle bundle) {
+                super.onCreate(bundle);
+                setContentView(R.layout.main);
 
-		/* Video Surface View */
-		/*
-		mPreview = (SurfaceView) findViewById(R.id.surface);
-		holder = mPreview.getHolder();
-		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-		*/
-		/* Accelerometer */ 
-		/*
-		// Get an instance of the SensorManager
-		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-		// Get an instance of the PowerManager
-		mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
-		// Get an instance of the WindowManager
-		mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-		mDisplay = mWindowManager.getDefaultDisplay();
-		// Create a bright wake lock
-		mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, getClass()
-				.getName());
+                /* Video Surface View */
+                /*
+                mPreview = (SurfaceView) findViewById(R.id.surface);
+                holder = mPreview.getHolder();
+                holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+                */
+                /* Accelerometer */
+                /*
+                // Get an instance of the SensorManager
+                mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+                // Get an instance of the PowerManager
+                mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
+                // Get an instance of the WindowManager
+                mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+                mDisplay = mWindowManager.getDefaultDisplay();
+                // Create a bright wake lock
+                mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, getClass()
+                                .getName());
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
-		*/
-		
-		this.ipAddress = "";
-		this.portNumber = 0;
+                */
 
-		/* Show ready dialog to get IP and port number of server */
-		Intent i = new Intent(this, TextEntryActivity.class);
-		this.startActivityForResult(i, EDIT_ACTION);
-		
-		/* TCP client network task */
-		//Create initial instance so SendDataToNetwork doesn't throw an error.
-		networktask = new NetworkTask(); 
-		if (this.ipAddress.length() > 0 && this.portNumber >0)
-			networktask.execute(this.ipAddress, new Integer(this.portNumber).toString());
-	}
-	
-	private void roombaForward(){
-		networktask.SendDataToNetwork("w"); 
-	}
-	
-	private void roombaBackward(){
-		networktask.SendDataToNetwork("s"); 
-	}
-	
-	private void roombaLeftSpin(){
-		networktask.SendDataToNetwork("a"); 
-	}
-	
-	private void roombaRightSpin(){
-		networktask.SendDataToNetwork("d"); 
-	}
-	
-	/* Moving Roomba */
-	public void forwardButtonListener(View v) {
-		roombaForward();
-	}
+                this.ipAddress = "";
+                this.portNumber = 0;
 
-	public void backwardButtonListener(View v) {
-		roombaBackward();
-	}
+                /* Show ready dialog to get IP and port number of server */
+                Intent i = new Intent(this, TextEntryActivity.class);
+                this.startActivityForResult(i, EDIT_ACTION);
 
-	public void leftSpinButtonListener(View v) {
-		roombaLeftSpin();
-	}
+                /* TCP client network task */
+                //Create initial instance so SendDataToNetwork doesn't throw an error.
+                networktask = new NetworkTask();
+                if (this.ipAddress.length() > 0 && this.portNumber >0)
+                        networktask.execute(this.ipAddress, new Integer(this.portNumber).toString());
+        }
 
-	public void rightButtonListener(View v) {
-		roombaRightSpin();
-	}
-	
-	public void stopButtonListener(View v) {
-		roombaStop();
-	}
-	
-	private void roombaStop() {
-		// TODO Auto-generated method stub
-		
-	}
+        private void roombaForward(){
+                networktask.SendDataToNetwork("w");
+        }
 
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {		
-	}
+        private void roombaBackward(){
+                networktask.SendDataToNetwork("s");
+        }
 
-	@Override
-	public void onSensorChanged(SensorEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+        private void roombaLeftSpin(){
+                networktask.SendDataToNetwork("a");
+        }
+
+        private void roombaRightSpin(){
+                networktask.SendDataToNetwork("d");
+        }
+
+        /* Moving Roomba */
+        public void forwardButtonListener(View v) {
+                roombaForward();
+        }
+
+        public void backwardButtonListener(View v) {
+                roombaBackward();
+        }
+
+        public void leftSpinButtonListener(View v) {
+                roombaLeftSpin();
+        }
+
+        public void rightButtonListener(View v) {
+                roombaRightSpin();
+        }
+
+        public void stopButtonListener(View v) {
+                roombaStop();
+        }
+
+        private void roombaStop() {
+                // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent arg0) {
+                // TODO Auto-generated method stub
+
+        }
 }
